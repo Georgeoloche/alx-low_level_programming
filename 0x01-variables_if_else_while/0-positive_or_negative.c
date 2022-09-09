4dc427907150d4780f8148dc5b1964ad8a7acb54
Georgeoloche <95925952+Georgeoloche@users.noreply.github.com>
2022-09-09 14:03:55 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 
 /**
  * main - Print a random number and states weather
@@ -19,7 +19,7 @@
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+	if (n > 0)
            printf("%d is positive\n", n);
         else if (n < 0)
                 printf("%d is negative\n", n);
